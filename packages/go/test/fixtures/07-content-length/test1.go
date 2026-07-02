a9e80e968c398953eb3bad032effb299cf51ff06
datadog-bits <263423550+datadog-bits@users.noreply.github.com>
2026-07-02 13:49:28 +0000

    Escape env output in Go fixture

--- packages/go/test/fixtures/07-content-length/test1.go
+++ packages/go/test/fixtures/07-content-length/test1.go
@@ -1,6 +1,7 @@
 package function
 
 import (
+	"html"
 	"net/http"
 	"os"
 	"strconv"
@@ -9,8 +10,9 @@ import (
 // HandlerTest1 function
 func HandlerTest1(w http.ResponseWriter, r *http.Request) {
 	rdm := os.Getenv("RANDOMNESS_ENV_VAR")
+	body := html.EscapeString(rdm) + ":content-length"
 
 	w.WriteHeader(401)
-	w.Header().Set("content-length", strconv.Itoa(len(rdm+":content-length")))
-	w.Write([]byte(rdm + ":content-length"))
+	w.Header().Set("content-length", strconv.Itoa(len(body)))
+	w.Write([]byte(body))
 }
